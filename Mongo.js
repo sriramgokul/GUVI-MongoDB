9c7498aad9c3f8020b815369d7c52303cd5997c3
SRIRAM GOKUL <sriramgokul22@gmail.com>
2024-09-27 07:55:23 +0530

    Created a js file for mogodb

---
+++ Mongo.js
@@ -0,0 +1,6 @@
+// ****** Flow of MongoDB--Database->Collections->Document *****
+
+// To show the list of databases --> show dbs
+// To shift to any database --> use databasename
+// To show the list of Collections in the database --> show collections
+// To create a new Database --> use newDatabase Name
